b42abe60a22c16fedac68421c104b3103d558ec6
LukaszKocioll <kociollas@wp.pl>
2023-10-28 13:51:23 +0200

    Added saving contacts in local storage and updating readme

--- src/components/PhoneBook/PhoneBook.jsx
+++ src/components/PhoneBook/PhoneBook.jsx
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import { ContactForm } from './PhoneBookContactForm';
 import { ContactList } from './PhoneBookContactList';
 
-
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      this.setState({ contacts: JSON.parse(storedContacts) });
+    }
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
 
   handleAddContact = (newContact) => {
     this.setState((prevState) => ({
